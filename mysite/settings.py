dddc35c6637826ed8f9eb505dc098167c84a71af
Shivateja Burle <shivateja.idizital@gmail.com>
2025-04-16 18:48:16 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,8 +82,8 @@ CURRENCY_API_KEY = os.environ.get("CURRENCY_API_KEY")
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
